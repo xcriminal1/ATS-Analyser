1b18ea2157b403d29b1c9099edd5f92119236587
Aman Singh <aaryanamanaa19@gmail.com>
2025-03-30 01:09:38 +0530

    Update footer  app.py

--- app.py
+++ app.py
@@ -296,11 +296,11 @@ st.markdown("""
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <div class="footer">
-        <p>Developed by Anubhav Raj</p>
+        <p>Developed by Aman Singh</p>
         <p class="social-icons">
-            <a href="https://github.com/Anubhx" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://linkedin.com/in/anubhax/" target="_blank"><i class="fab fa-linkedin"></i></a>
-            <a href="mailto:anubhav0427@gmail.com"><i class="fas fa-envelope"></i></a>
+            <a href="https://github.com/xcriminal1" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://linkedin.com/in/amansingh-xcriminal/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="mailto:aaryanamanaa19@gmail.com@gmail.com"><i class="fas fa-envelope"></i></a>
         </p>
     </div>
 """, unsafe_allow_html=True)
